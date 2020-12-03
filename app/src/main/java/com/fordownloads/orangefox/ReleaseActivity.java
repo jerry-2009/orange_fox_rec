e1a4f853ecd0a86ecabb7595e4790269d1d1a9a6
fordownloads <vnukov.d03@gmail.com>
2020-12-04 03:12:06 +0800

    Add fancy animations to release info

--- app/src/main/java/com/fordownloads/orangefox/ReleaseActivity.java
+++ app/src/main/java/com/fordownloads/orangefox/ReleaseActivity.java
@@ -1,10 +1,13 @@
 package com.fordownloads.orangefox;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ProgressBar;
 
 import androidx.appcompat.app.ActionBar;
@@ -115,8 +118,16 @@ public class ReleaseActivity extends AppCompatActivity {
                     viewPagerTab.setViewPager(viewPager);
 
 
-
-                    findViewById(R.id.spinner).setVisibility(View.GONE);
+                    FrameLayout loadingView = findViewById(R.id.loadingLayout);
+                    loadingView.animate()
+                            .alpha(0f)
+                            .setDuration(200)
+                            .setListener(new AnimatorListenerAdapter() {
+                                @Override
+                                public void onAnimationEnd(Animator animation) {
+                                    loadingView.setVisibility(View.GONE);
+                                }
+                            });
                 }});
         } catch (JSONException e) {
             e.printStackTrace();
