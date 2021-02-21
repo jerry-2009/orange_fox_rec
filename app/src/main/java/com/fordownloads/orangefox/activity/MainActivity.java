b47f79862b63b309dfdddf5b91906b07bf6d2a5c
fordownloads <vnukov.d03@gmail.com>
2021-02-21 16:21:52 +0800

    Optimize page switching code

--- app/src/main/java/com/fordownloads/orangefox/activity/MainActivity.java
+++ app/src/main/java/com/fordownloads/orangefox/activity/MainActivity.java
@@ -26,10 +26,18 @@ import com.fordownloads.orangefox.consts;
 import com.topjohnwu.superuser.Shell;
 
 public class MainActivity extends AppCompatActivity {
+    LogsFragment logs;
+    ScriptsFragment scripts;
+    InstallFragment install;
+
+    PRDownloaderConfig config = PRDownloaderConfig.newBuilder()
+            .setDatabaseEnabled(true)
+            .setReadTimeout(30_000)
+            .setConnectTimeout(30_000)
+            .build();
+
     static {
-        Shell.setDefaultBuilder(Shell.Builder.create()
-                .setFlags(Shell.FLAG_REDIRECT_STDERR)
-                .setTimeout(10));
+        Shell.setDefaultBuilder(Shell.Builder.create().setFlags(Shell.FLAG_REDIRECT_STDERR).setTimeout(10));
         try {
             StrictMode.class.getMethod("disableDeathOnFileUriExposure").invoke(null);
         } catch (Exception ignored) {}
@@ -40,15 +48,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        createNotificationChannel();
-
-        PRDownloaderConfig config = PRDownloaderConfig.newBuilder()
-                .setDatabaseEnabled(true)
-                .setReadTimeout(30_000)
-                .setConnectTimeout(30_000)
-                .build();
         PRDownloader.initialize(getApplicationContext(), config);
-
+        createNotificationChannel();
         prepareBottomNav();
 
         getWindow().setNavigationBarColor(ContextCompat.getColor(this, R.color.fox_status_solid_bg));
@@ -56,28 +57,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void createNotificationChannel() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationManager notifyMan = getSystemService(NotificationManager.class);
+        NotificationManager notifyMan = getSystemService(NotificationManager.class);
 
-            NotificationChannel channel = new NotificationChannel(consts.CHANNEL_UPDATE, getString(R.string.notif_ch_update), NotificationManager.IMPORTANCE_HIGH);
-            channel.setDescription(getString(R.string.notif_ch_update_desc));
-            notifyMan.createNotificationChannel(channel);
+        NotificationChannel channel = new NotificationChannel(consts.CHANNEL_UPDATE, getString(R.string.notif_ch_update), NotificationManager.IMPORTANCE_HIGH);
+        channel.setDescription(getString(R.string.notif_ch_update_desc));
+        notifyMan.createNotificationChannel(channel);
 
-            channel = new NotificationChannel(consts.CHANNEL_DOWNLOAD, getString(R.string.notif_ch_download), NotificationManager.IMPORTANCE_LOW);
-            channel.setDescription(getString(R.string.notif_ch_download_desc));
-            notifyMan.createNotificationChannel(channel);
+        channel = new NotificationChannel(consts.CHANNEL_DOWNLOAD, getString(R.string.notif_ch_download), NotificationManager.IMPORTANCE_LOW);
+        channel.setDescription(getString(R.string.notif_ch_download_desc));
+        notifyMan.createNotificationChannel(channel);
 
-            channel = new NotificationChannel(consts.CHANNEL_DOWNLOAD_STATUS, getString(R.string.notif_ch_download_status), NotificationManager.IMPORTANCE_HIGH);
-            channel.setDescription(getString(R.string.notif_ch_download_status_desc));
-            notifyMan.createNotificationChannel(channel);
-        }
+        channel = new NotificationChannel(consts.CHANNEL_DOWNLOAD_STATUS, getString(R.string.notif_ch_download_status), NotificationManager.IMPORTANCE_HIGH);
+        channel.setDescription(getString(R.string.notif_ch_download_status_desc));
+        notifyMan.createNotificationChannel(channel);
     }
 
     protected void prepareBottomNav() {
         AHBottomNavigation bn = findViewById(R.id.bottom_navigation);
         bn.addItem(new AHBottomNavigationItem(R.string.bnav_install, R.drawable.ic_round_save_alt_24, R.color.fox_accent));
         bn.addItem(new AHBottomNavigationItem(R.string.bnav_scripts, R.drawable.ic_outline_receipt_long_24, R.color.fox_accent));
-        //bn.addItem(new AHBottomNavigationItem(R.string.bnav_backups, R.drawable.ic_outline_cloud_download_24, R.color.fox_accent));
         bn.addItem(new AHBottomNavigationItem(R.string.bnav_logs, R.drawable.ic_commit, R.color.fox_accent));
 
         bn.setBehaviorTranslationEnabled(false);
@@ -90,57 +88,24 @@ public class MainActivity extends AppCompatActivity {
         bn.setTitleTypeface(ResourcesCompat.getFont(getApplicationContext(), R.font.euclid_flex));
         bn.setTitleTextSizeInSp(14, 12);
         bn.setTitleState(AHBottomNavigation.TitleState.ALWAYS_SHOW);
+        bn.setOnTabSelectedListener(this::switchPages);
 
         FragmentManager fm = getSupportFragmentManager();
-
-        if (fm.findFragmentByTag("install") == null)
-            fm.beginTransaction().add(R.id.nav_frame, new InstallFragment(), "install").commit();
-
-        if (fm.findFragmentByTag("scripts") == null) {
-            ScriptsFragment scripts = new ScriptsFragment();
-            fm.beginTransaction().add(R.id.nav_frame, scripts, "scripts").commit();
-            fm.beginTransaction().hide(scripts).commit();
-        }
-        if (fm.findFragmentByTag("logs") == null) {
-            LogsFragment logs = new LogsFragment();
-            fm.beginTransaction().add(R.id.nav_frame, logs, "logs").commit();
-            fm.beginTransaction().hide(logs).commit();
-        }
-
-        bn.setOnTabSelectedListener((position, wasSelected) -> {
-            if (wasSelected)
-                return true;
-            FragmentTransaction tsa = fm.beginTransaction();
-            tsa.setCustomAnimations(R.anim.scale, R.anim.scale);
-
-            switch (position) {
-                case 2:
-                    if(fm.findFragmentByTag("logs") != null) tsa.show(fm.findFragmentByTag("logs")).commit();
-                    else tsa.add(R.id.nav_frame, new LogsFragment(), "logs").commit();
-
-                    if(fm.findFragmentByTag("scripts") != null) fm.beginTransaction().hide(fm.findFragmentByTag("scripts")).commit();
-                    if(fm.findFragmentByTag("install") != null) fm.beginTransaction().hide(fm.findFragmentByTag("install")).commit();
-
-                    break;
-                case 1:
-                    if(fm.findFragmentByTag("scripts") != null) tsa.show(fm.findFragmentByTag("scripts")).commit();
-                    else tsa.add (R.id.nav_frame, new ScriptsFragment(), "scripts").commit();
-
-                    if(fm.findFragmentByTag("install") != null) fm.beginTransaction().hide(fm.findFragmentByTag("install")).commit();
-                    if(fm.findFragmentByTag("logs") != null) fm.beginTransaction().hide(fm.findFragmentByTag("logs")).commit();
-
-                    break;
-                default:
-                    if(fm.findFragmentByTag("install") != null) tsa.show(fm.findFragmentByTag("install")).commit();
-                    else tsa.add (R.id.nav_frame, new InstallFragment(), "install").commit();
-
-                    if(fm.findFragmentByTag("scripts") != null) fm.beginTransaction().hide(fm.findFragmentByTag("scripts")).commit();
-                    if(fm.findFragmentByTag("logs") != null) fm.beginTransaction().hide(fm.findFragmentByTag("logs")).commit();
-
-                    break;
-            }
-            return true;
-        });
+        if (install == null)
+            fm.beginTransaction().add(R.id.nav_frame, (install = new InstallFragment())).commit();
+        if (scripts == null)
+            fm.beginTransaction().add(R.id.nav_frame, (scripts = new ScriptsFragment())).hide(scripts).commit();
+        if (logs == null)
+            fm.beginTransaction().add(R.id.nav_frame, (logs = new LogsFragment())).hide(logs).commit();
     }
 
+    private boolean switchPages(int position, boolean wasSelected) {
+        if (wasSelected) return false;
+        FragmentTransaction tsa = getSupportFragmentManager().beginTransaction().setCustomAnimations(R.anim.scale, 0);
+        switch (position) {
+            case 1:  tsa.show(scripts).hide(install).hide(logs).commit(); return true;
+            case 2:  tsa.show(logs).hide(install).hide(scripts).commit(); return true;
+            default: tsa.show(install).hide(scripts).hide(logs).commit(); return true;
+        }
+    }
 }
